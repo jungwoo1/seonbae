9b16f8d09cd59b7a22b3dfc2edb04274a5024a28
skydkakwhs <skydkakwhs@gmail.com>
2021-06-10 12:21:51 +0900

    실습중

---
+++ src/views/AskView.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        
+        <p v-for="item in this.$store.state.jobs">
+        <a v-bind:href="item.url">
+            {{item.title}}</a>
+            <small>{{item.time_ago}},{{item.domain}}</small>
+            </p>
+    </div>
+</template>
+
+<script> 
+import { mapState } from 'vuex'
+export default {
+    computed: {
+        ...mapState({
+            ask: state => state.ask
+        })
+    },
+    
+    created() {
+        this.$store.dispatch('FETCH_ASK')
+      
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
